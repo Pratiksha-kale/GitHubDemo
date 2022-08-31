2c50098718fdf98a343609b2c469a73a1a9269e6
Admin <kalepratiksha27@gmail.com>
2022-08-31 17:12:28 +0530

    add demo1

--- GitHubDemo/src/PimPage/Demo1.java
+++ GitHubDemo/src/PimPage/Demo1.java
@@ -1,10 +1,10 @@
 package PimPage;
 
-public class PimpageTest {
+public class Demo1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("pim page test");
+System.out.println("welcome");
 	}
 
 }
